ef303502ce89d0c26f395d618fe0de5f5a4eb7f4
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-25 20:13:26 +0530

    getTodoById created

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -1,18 +1,52 @@
 const Todo = require("../models/Todo");
 
+// Define route handler
 exports.getTodo = async (req, res) => {
   try {
+    // fetch all todo items from the database
     const todos = await Todo.find({});
+
+    //response
     res.status(200).json({
       success: true,
       data: todos,
-      message: "All todos fetched",
+      message: "All Todos are fetched successfully.",
     });
   } catch (err) {
     console.error(err);
     res.status(500).json({
       success: false,
-      message: "server error",
+      message: "Server Error",
+    });
+  }
+};
+
+exports.getTodoById = async (req, res) => {
+  try {
+    // Extract todo item based on id
+    const { id } = req.params;
+    // const id = req.params.id;
+    const todo = await Todo.findById({ _id: id });
+
+    // if data is not found
+    if (!todo) {
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found with given id",
+      });
+    }
+
+    //data is found
+    res.status(200).json({
+      success: true,
+      data: todo,
+      message: `todo ${id} data found successfully`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
     });
   }
 };
--- routes/todos.js
+++ routes/todos.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 // Import controllers
 const { createTodo } = require("../controllers/createTodo");
-const { getTodo } = require("../controllers/getTodo");
+const { getTodo, getTodoById } = require("../controllers/getTodo");
 
 // Define API routes
 router.post("/createTodo", createTodo);
 router.get("/getTodo", getTodo);
+router.get("/getTodo/:id", getTodoById);
 
 module.exports = router;
