480a6cc3090d6e5e68a23ed8860420de3f7d082c
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-25 13:11:35 +0530

    index updated

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-
-
-const express = require('express');
+const express = require('express')
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("App successful");
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+const todoRoutes = require("./routes/todos");
+
+app.use("api/v1", todoRoutes);
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at PORT ${PORT}`);
+})
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>This is HOMEPAGE</h1>`);
 })
